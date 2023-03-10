2b91417683b92cf1cb66d62b1798529c94537cb0
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-10 16:09:09 +0100

    Add files via upload

--- src/statics/UiConfig.js
+++ src/statics/UiConfig.js
@@ -1,8 +1,3 @@
-// This config is used in both the
-// frontend as well as the web server.
-
-// see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
-
 const CONFIG = {
   headless: false,
   api: {
@@ -17,16 +12,7 @@ const CONFIG = {
     path: "/"
   },
   adapter: "sqlite",
-
-  /**
-   * Gordon UI - configure your additional Indicator names here
-   * (standard TA-Lib and Tulip ones are already defined)
-   * patterns: for Pattern-Recognizing indicators
-   * indicators: for RSI and so on - should not be displayed as Overlay
-   * overlays: all Indicators that can be put into the main-chart as overlay, for Example SMA, EMA, Bollinger-Bands etc.
-   * Example-Configuration done for tulip-macd - strat
-   * If name on chart contains an '_', add the name after the '_' to this array.
-   */
+  
   userChartConfig: {
     //patterns:['hasInvertedHammer']
     indicators: [
