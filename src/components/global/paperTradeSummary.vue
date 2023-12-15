d7a4498737643a859101339aa1d13d3cb1858cda
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-12-15 17:17:52 +0000

    Add files via upload

---
+++ src/components/global/paperTradeSummary.vue
@@ -0,0 +1,32 @@
+<template>
+  <dl class="horizontal">
+    <dt>Amount of trades</dt>
+    <dd>{{report.trades}}</dd>
+    <dt>Sharpe ratio</dt>
+    <dd>{{round2(report.sharpe)}}</dd>
+    <dt>Start Balance</dt>
+    <dd>{{round(report.startBalance)}} {{report.currency}}</dd>
+    <dt>End Balance</dt>
+    <dd>{{round(report.balance)}} {{report.currency}}</dd>
+    <dt>Simulated profit</dt>
+    <dd class="q-title" :class="profitClass">{{round(report.relativeProfit)}} %</dd>
+  </dl>
+</template>
+
+<script>
+export default {
+  props: ['report'],
+  methods: {
+    round2: n => (+n).toFixed(2),
+    round: n => (+n).toFixed(5)
+  },
+  computed: {
+    profitClass: function() {
+      if(this.report.relativeProfit > 0)
+        return 'text-positive'
+      else
+        return 'text-negative'
+    }
+  }
+}
+</script>
