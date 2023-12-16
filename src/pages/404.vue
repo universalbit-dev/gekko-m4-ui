1b03fa63c75727316080d1aab9487147b9ea4155
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-12-16 11:03:28 +0100

    Update 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,7 +2,7 @@
   <div class="fixed-center text-center">
     <p>
       <img
-        src="~assets/sad.svg"
+        src="~assets/quasar-logo-full.svg"
         style="width:30vw;max-width:150px;"
       >
     </p>
